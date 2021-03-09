eff5323e2b2380fc20834c1f95e402a7989a4ab4
SoumitriM <mail.soumitri@gmail.com>
2021-03-09 11:08:39 +0530

    Added ASP.NET Core projects

---
+++ EmptyMvcCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmptyMvcCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
