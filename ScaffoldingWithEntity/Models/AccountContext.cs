090ed32c3b373544c7b15da3e0f0212e8bd9d717
SoumitriM <mail.soumitri@gmail.com>
2021-03-10 14:49:43 +0530

    Added Entity project

---
+++ ScaffoldingWithEntity/Models/AccountContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScaffoldingWithEntity.Models
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
