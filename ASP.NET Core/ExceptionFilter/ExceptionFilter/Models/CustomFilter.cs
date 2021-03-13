2a70b8ff597cea5e474b531bc38a10e0cef54a88
SoumitriM <mail.soumitri@gmail.com>
2021-03-13 21:32:47 +0530

    Logging with ASP.Net Core project

---
+++ ASP.NET Core/ExceptionFilter/ExceptionFilter/Models/CustomFilter.cs
@@ -0,0 +1,30 @@
+﻿using log4net.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionFilter.Models
+{
+    public class CustomFilter:IExceptionFilter
+    {
+         
+
+        public void OnException(ExceptionContext context)
+        {
+            var result = new ViewResult { ViewName = "CustomException" };
+            var modelMetaData = new EmptyModelMetadataProvider();
+            result.ViewData = new ViewDataDictionary(modelMetaData, context.ModelState);
+            result.ViewData.Add("HandleException", context.Exception);
+            context.Result = result;
+            context.ExceptionHandled = true;
+            LoggerManager loggerManager = new LoggerManager();
+            loggerManager.LogInformation("Exception : " + context.Exception.Message);
+        }
+    }
+}
+
---
+++ ASP.NET Core/ExceptionFilter/ExceptionFilter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExceptionFilter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
